03d343c5ca71b92b3005e262f4454ff1867d64b3
SaadyBennani <sbennani23@gmail.com>
2019-05-24 16:37:39 -0700

    search_results

--- restaurant_roulette/resto_roulette/urls.py
+++ restaurant_roulette/resto_roulette/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'resto_roulette'  # for namespacing
 urlpatterns = [
-    path('home', views.index, name='home_view'),
-    path('', views.Business_search, name='biz_search'),
-    path('search-results', views.search_results, name='search_results'),
-    path('cards', views.cards, name='cards'),
+    path('', views.index, name='home_view'),
+    path('search', views.Business_search, name='biz_search'),
+    path('queue', views.queue, name='queue'),
+
 ]
--- restaurant_roulette/resto_roulette/views.py
+++ restaurant_roulette/resto_roulette/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Business_Search
-from .forms import BusinessForm
-from .YelpAPI import API_KEY
 import requests
+import json
 
 
 # Create your views here.
@@ -12,55 +11,37 @@ import requests
 def index(request):
     return render(request, 'resto_roulette/index.html')
 
-def cards(request):
-    return render(request, 'resto_roulette/cards.html')
+def queue(request):
+    if request.method == 'POST':
+        biz_queue = json.loads(request.body)
+        # response = JsonResponse(biz_queue, safe=False)
+        context = {
+            'biz_queue': JsonResponse(biz_queue, safe=False)
+        }
+        print(context)
+        print(biz_queue)
+        return render(request, 'resto_roulette/queue.html', context)
+    return render(request, 'resto_roulette/queue.html')
 
-def search_results(request):
-    pass
+
+
+
 
 def Business_search(request):
-    form = BusinessForm(request.POST or None)
     if request.method == 'POST':
-        search_results = []
-        if form.is_valid():
-            cat = request.POST['biz_category']
-            loc = request.POST['location']
-            search = Business_Search(biz_category=cat, location=loc)
-            form = BusinessForm()
-
-            business = []
-
-            search_results = API_search(cat, loc)
-            print(search_results)
-            review_results_total = []
-
-            for idx, business in enumerate(search_results):
-                biz_id = business['id']
-                review_results = Review_search(biz_id)
-                search_results[idx]['reviews'] = review_results
-                review_results_total.append(review_results)
-            print(review_results)
-
+        print(request.POST)
         context = {
-            'form': form,
-            'search_results': search_results,
-            'businesses': [],
-            'review_results_total': review_results_total,
+            'what': request.POST.get('what'),
+            'where': request.POST.get('where')
         }
+
+        print(context['what'])
+        print(context['where'])
+
+
         return render(request, 'resto_roulette/search_results.html', context)
 
-    return render(request, 'resto_roulette/biz_search.html', {'form': form})
-
-
-def API_search(cat, loc):
-    endpoint = 'https://api.yelp.com/v3/businesses/search'
-    headers = {'Authorization': 'bearer %s' % API_KEY}
-    parameters = {'term': cat, 'location': loc,
-                  'limit': 5,
-                  'radius': 10000}
-    response = requests.get(url=endpoint, params=parameters, headers=headers)
-    business_data = response.json()
-    return business_data['businesses']
+    return render(request, 'resto_roulette/biz_search.html')
 
 
 def Review_search(biz_id):
